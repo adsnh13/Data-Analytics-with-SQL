87ec19460f699d5b23ad549e935daae8456c0533
Aaditya Sinha <13adsinha@gmail.com>
2025-08-20 17:00:10 +0530

    Update and rename 13_report_products.sql to 13 Report Products.sql

--- 13 Report Products.sql
+++ 13 Report Products.sql
@@ -1,25 +1,4 @@
-/*
-===============================================================================
-Product Report
-===============================================================================
-Purpose:
-    - This report consolidates key product metrics and behaviors.
 
-Highlights:
-    1. Gathers essential fields such as product name, category, subcategory, and cost.
-    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
-    3. Aggregates product-level metrics:
-       - total orders
-       - total sales
-       - total quantity sold
-       - total customers (unique)
-       - lifespan (in months)
-    4. Calculates valuable KPIs:
-       - recency (months since last sale)
-       - average order revenue (AOR)
-       - average monthly revenue
-===============================================================================
-*/
 -- =============================================================================
 -- Create Report: gold.report_products
 -- =============================================================================
@@ -110,5 +89,6 @@ SELECT
 		WHEN lifespan = 0 THEN total_sales
 		ELSE total_sales / lifespan
 	END AS avg_monthly_revenue
+
 
 FROM product_aggregations 
